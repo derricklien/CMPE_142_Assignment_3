484d37983b20bc23318acfa27a5a144f210559f6
Salvador Rodriguez <salvador.rodriguez01@sjsu.edu>
2019-05-10 14:39:24 -0700

    different approach

--- main.cpp
+++ main.cpp
@@ -4,30 +4,41 @@
 #include <string>
 #include <fstream>
 #include <vector>
-
 using namespace std;
+
+struct PhysicalMemory{
+    int owner;
+    int virAddress;
+    bool accessed;
+    bool dirty;
+};
+
+struct swapMemory{
+    int owner;
+    int virAddress;
+    bool accessed;
+    bool dirty;
+};
 
 int main()
 {
     vector <int> process_ID;
     vector <char> action;
     vector <int> page;
-    vector <int> addPhysicalMem;
-    vector <int> addPhysicalProc;
-    vector <int> freePhysicalMem;
-    vector <int> freePhysicalProc;
+    vector <PhysicalMemory> physcial;
+    vector <int> test;
+    vector <swapMemory> swap;
     
     int idHolder=0;
     int pageHolder=0;
     char actHolder = 'Z';
-    int entry = 0; // num must start at 0
+    int entry = 0;
+    
     int createCount = 0;
     int terminateCount = 0;
-    int physPages [19] = {0};
+    
     string line;
     ifstream file ("cat.txt");  // file containing numbers in 3 columns //replace cat.txt later
-    //    cout << "original: " <<endl;
-    
     //reading and stroing file data
     if(file.is_open()){ // checks to see if file opened
         
@@ -44,16 +55,60 @@ int main()
         }
         file.close();
     }
-    cout << "Num is:"<< entry<<endl;
-    for (int i  = 0; i < entry; i++ ){
-        cout<< "PID: "<< process_ID[i] << " Act: " << action[i]<< " Page: " << page[i]<< " i:"<<i <<endl;
-    }
-    int addSize = 0;
-    int freeSize = 0;
-    //FIFO
     
+    cout << "Num is:"<< entry<<endl<< "\t\tOriginal\n";
+    
+    for (int i  = 0; i < entry; i++ ){
+        cout<< "\tPID: "<< process_ID[i] << " Act: " << action[i]<< " Page: " << page[i]<< " i:"<<i <<endl;// debug print
+    }
+    
+    int PhysSize = 0;
+    int SwapSize = 0;
+    cout << "PS was: "<<PhysSize<<endl;
+    cout << "SS was: "<<SwapSize<<endl;
+
+    //FIFO ... Still working on this but kept it in here so you can see how to use the struct as a vector
+    for (int i =0; i < entry; i++){
+        if (PhysSize < 20){
+            if (action[i] == 'A'){
+                physcial.push_back(PhysicalMemory());
+                physcial[PhysSize].owner = process_ID[i];
+                physcial[PhysSize].virAddress = page[i];
+                PhysSize = physcial.size();
+            }
+        }
+        if (PhysSize >= 20){
+            if (action[i] == 'A'){
+                cout << "Need to swap No room for PID: " << process_ID[i] << " Page: "<< page[i]<<endl; // debug print
+                swap.push_back(swapMemory());
+                swap[SwapSize].owner = process_ID[i];
+                swap[SwapSize].virAddress = page[i];
+                SwapSize = swap.size();
+            }
+        }
+        if (action[i] == 'F'){
+            cout << "Need to check and free PID: "<< process_ID[i] << " Page: "<< page[i]<<endl;// debug print
+            for (int  k = 0; k < PhysSize; k++){
+                if(process_ID[i] == physcial[k].owner && page[i] == physcial[k].virAddress)
+                {
+                    cout << "Found at: "<< k<<endl;// debug print
+                    physcial.erase(physcial.begin() + k);
+                    PhysSize = physcial.size();
+                }
+            }
+        }
+    }
+    cout << "PS now: "<<PhysSize<<endl;
+    
+    cout << "\tPhysical Memory\n";
+    for(int j =0; j <PhysSize; j ++){
+        cout <<"\tPM: "<< physcial[j].owner << " VA: " << physcial[j].virAddress<<  " at: "<<j  << endl;// debug print
+    }
+    cout << "\tSwap Memory\n";
+
+    for(int j =0; j <SwapSize; j ++){
+        cout <<"\tSM: "<< swap[j].owner << " VA: " << swap[j].virAddress<<  " at: "<<j  << endl;// debug print
+    }
+
     return 0;
 }
-
-
-
